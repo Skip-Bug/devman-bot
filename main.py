891ee237e6c39fd0e92489b4a52451e91fae8183
Leonid Stoyko <gribojor@gmail.com>
2026-06-09 16:48:30 +0500

    add func

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
 import os
 import pprint
+import time
+
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
 
-# url = 'https://dvmn.org/api/user_reviews/'
-url = 'https://dvmn.org/api/long_polling/'
-token = os.getenv('TOKEN')
-headers = {'Authorization': f'Token {token}'}
-response = requests.get(url, headers=headers, timeout=95)
-response.raise_for_status()
+def check_has_review(url, token, timestamp=None):
+    headers = {'Authorization': f'Token {token}'}
+    params = {'timestamp': timestamp}
+    if not timestamp:
+        response = requests.get(url, headers=headers, timeout=95)
+    else:
+        response = requests.get(
+            url, headers=headers,
+            timeout=95, params=params
+        )
 
-response_json = response.json()
-pprint.pprint(response_json)
+    response.raise_for_status()
+    return response.json()
 
 
+def main():
+    load_dotenv()
+
+    # url = 'https://dvmn.org/api/user_reviews/'
+    url = 'https://dvmn.org/api/long_polling/'
+    token = os.getenv('TOKEN')
+    timestamp = None
+    while True:
+        try:
+            review_status = check_has_review(url, token, timestamp)
+            print('Результат запроса:')
+            pprint.pprint(review_status)
+            print()
+            if review_status.get('status') == "found":
+                timestamp = review_status.get('last_attempt_timestamp')
+            elif review_status.get('status') == "timeout":
+                timestamp = review_status.get('timestamp_to_request')
+
+        except requests.exceptions.RequestException:
+            time.sleep(10)
+            print('Попытаемся еще раз')
+            continue
+
+        except KeyboardInterrupt:
+            print('Остановленно пользователем')
+            break
+
+
+if __name__ == '__main__':
+    main()
