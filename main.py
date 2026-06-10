c36064be0ac806048ce13b9937a97ab83f740798
Leonid Stoyko <gribojor@gmail.com>
2026-06-10 13:11:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
-import pprint
 import time
 
 import requests
 from dotenv import load_dotenv
+from telegram import Bot
 
 
 def check_has_review(url, token, timestamp=None):
@@ -26,30 +26,36 @@ def main():
 
     # url = 'https://dvmn.org/api/user_reviews/'
     url = 'https://dvmn.org/api/long_polling/'
+    tg_token = os.getenv('TG_BOT_TOKEN')
     token = os.getenv('DEVMAN_TOKEN')
+    if not token or not tg_token:
+        return
     timestamp = None
+    bot = Bot(token=tg_token)
+
+    username = os.getenv('TG_USERNAME')
+    chat_id = os.getenv('TG_CHAT_ID')
+    
     while True:
         try:
             review_status = check_has_review(url, token, timestamp)
-            print('Результат запроса:')
-            pprint.pprint(review_status)
-            print()
             if review_status.get('status') == "found":
                 timestamp = review_status.get('last_attempt_timestamp')
+                bot.send_message(
+                    chat_id=chat_id,
+                    text=f'@{username}, Преподаватель проверил работу!'                    
+                )
+
             elif review_status.get('status') == "timeout":
                 timestamp = review_status.get('timestamp_to_request')
 
         except requests.exceptions.ReadTimeout:
             time.sleep(5)
-            print('Попытаемся еще раз')
             continue
         except requests.exceptions.ConnectionError:
             time.sleep(5)
-            print('Попытаемся еще раз')
-            continue        
-        except KeyboardInterrupt:
-            print('Остановленно пользователем')
-            break
+            continue
+
 
 
 if __name__ == '__main__':
